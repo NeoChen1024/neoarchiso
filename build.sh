171a6236d86694b2908372d226076763d0c5f01c
Neo_Chen <chenkolei@gmail.com>
2019-07-24 19:41:38 +0800

    Changed iso filename

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e -u
 
-iso_name=archlinux
-iso_label="ARCH_$(date +%Y%m)"
+iso_name=neoarchiso
+iso_label="NEOARCHISO_$(date +%Y%m)"
 iso_publisher="Arch Linux <http://www.archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
